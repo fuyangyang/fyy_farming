899a5bc9090def7886d0d101454516be2ab43b60
fuyangyang <fuyangyang@meituan.com>
2018-08-05 16:23:33 +0800

    add mainclass demo

---
+++ src/main/java/mainclass/src/main/java/com/fyy/Starter.java
@@ -0,0 +1,7 @@
+package com.fyy;
+
+public class Starter {
+    public static void main(String[] args) {
+        System.out.println("start from mainclass pointed by the pom.xml shade...");
+    }
+}
