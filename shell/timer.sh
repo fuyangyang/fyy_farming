e5597466172eb694960abfd53e1074ae18c0d8ee
fuyangyang <fuyangyang@meituan.com>
2019-01-21 15:23:40 +0800

    add xargs tips

--- shell/timer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-start=$(date +%s)
-sleep 2
-end=$(date +%s)
-echo "cost seconds:"$(( $end - $start ))
--- src/main/java/com/BasicTest.java
+++ src/main/java/com/BasicTest.java
@@ -4,6 +4,8 @@ import java.text.ParseException;
 import java.util.HashMap;
 import java.util.Map;
 
+import static java.util.Calendar.LONG;
+
 public class BasicTest {
     public static void main1(String[] args) {
         Map<String, Float> map = new HashMap<>(2);
@@ -22,5 +24,10 @@ public class BasicTest {
         String s2 = null;
         System.out.println("case1: " + s1.equals(s2));
         System.out.println("case2: " + s2.equals(s1));
+    }
+
+    public static void main(String[] args) {
+//        -9223372036854775808L
+        System.out.println(Long.MIN_VALUE);
     }
 }
